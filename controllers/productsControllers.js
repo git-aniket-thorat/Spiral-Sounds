4eb7bda255942e30f1e95fe29cea19b2e0458d24
Aniket Thorat <thorataniket088@gmail.com>
2026-02-21 01:00:00 +0530

    add the data retriving part in it

--- controllers/productsControllers.js
+++ controllers/productsControllers.js
@@ -1,11 +1,44 @@
-export async function getGenres(){
+import { getDBConnection } from "../db/db.js";
 
-  console.log('genres')
+export async function getGenres(req, res) {
+  try {
+    const db = await getDBConnection();
 
+    const genreRows = await db.all("SELECT DISTINCT genre FROM products");
+    const genres = genreRows.map((row) => row.genre);
+    res.json(genres);
+  } catch (err) {
+    res
+      .status(500)
+      .json({ error: "Failed to fetch genres", details: err.message });
+    console.log(err);
+  }
 }
 
-export async function getProducts(){
+export async function getProducts(req, res) {
+  try {
+    const db = await getDBConnection();
+    let query =`SELECT * FROM products`
+    let params =[]
 
-  console.log('products')
+    const { genre,search }=req.query
 
+    if (genre ) {
+      query += ` WHERE genre = ? `
+      params.push(genre)
+    }else if(search){
+      query += ` WHERE title LIKE ?
+                OR artist LIKE ?
+                OR genre LIKE ?`
+      const match = `%${search}%`;
+      params.push(match,match,match)
+    }
+
+    const products = await db.all(query,params);
+    res.json(products);
+  } catch (err) {
+    res
+      .status(500)
+      .json({ error: "Failed to fetch products", details: err.message });
+  }
 }
