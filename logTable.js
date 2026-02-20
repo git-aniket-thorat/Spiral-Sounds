1c1ce48ea3d66870a5e0bd34fbefae5fbc77088d
Aniket Thorat <thorataniket088@gmail.com>
2026-02-20 13:01:36 +0530

    added the data.js and the seedTable
    added the data.js and the seedTable where in data.js there is data in object format and seedTable where query to insert the values present in data.js

--- logTable.js
+++ logTable.js
@@ -3,14 +3,18 @@ import { open } from 'sqlite'
 import path from 'node:path'
 
 async function viewAllProducts() {
-  const db = await open({
-    filename: path.join('database.db'), 
+  const db = await open({ 
+    filename: path.join('database.db'),
     driver: sqlite3.Database
   });
 
-  try {
+  try { 
     const products = await db.all('SELECT * FROM products')
-    console.table(products) 
+    // Neater table display
+    const displayItems = products.map(({ id, title, artist, year, stock }) => {
+      return { id, title, artist, year, stock }
+    })
+    console.table(displayItems)
   } catch (err) {
     console.error('Error fetching products:', err.message)
   } finally {
