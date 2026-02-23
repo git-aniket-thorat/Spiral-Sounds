0605c8697a23c791fc40b148b582690c2082caa3
Aniket Thorat <thorataniket088@gmail.com>
2026-02-23 21:57:32 +0530

    add the part of express session for login and how to logout user

--- sql/logTable.js
+++ sql/logTable.js
@@ -23,3 +23,28 @@ async function viewAllProducts() {
 }
 
 viewAllProducts()
+
+import { getDBConnection } from '../db/db.js'
+
+async function logTable() {
+  const db = await getDBConnection()
+
+  const tableName = 'users'
+
+  try { 
+
+    const table = await db.all(`SELECT * FROM ${tableName}`)
+    console.table(table)
+
+  } catch (err) {
+
+    console.error('Error fetching table:', err.message)
+
+  } finally {
+
+    await db.close()
+
+  }
+}
+
+logTable()
